c6eab9120c4a493d615b7d509360a24599c432e1
Seong Hyeri <gofl0721@withere.com>
2024-01-23 18:57:40 +0900

    #6.2 8:05~

--- src/Button.js
+++ src/Button.js
@@ -1,13 +1,13 @@
-import PropTypes from "prop-types"; //터미널에 npm i prop-types로 설치
+import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
-function Button({ text }) {
-  return <button className={styles.btn}>{text}</button>;
-  //검사창에서 보면 무작위적인 랜덤 class를 갖고있음
-}
+// function Button({ text }) {
+//   return <button className={styles.btn}>{text}</button>;
+//   //검사창에서 보면 무작위적인 랜덤 class를 갖고있음
+// }
 
-Button.propTypes = {
-  text: PropTypes.string.isRequired,
-};
+// Button.propTypes = {
+//   text: PropTypes.string.isRequired,
+// };
 
 export default Button;
