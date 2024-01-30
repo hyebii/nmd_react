f98d3c36679e44c86fdfa801b1d1ab9de8b5c0f0
Seong Hyeri <gofl0721@withere.com>
2024-01-30 18:52:34 +0900

    #7.0 ~6:38

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import { useState } from "react";
 
 function App() {
   const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
+  const onSubmit = (event) => { //양식제출이벤트 발생시
+    event.preventDefault(); //a태그나 submit태그를 누르면 href통해 이동하거나 새로고침되는데 그걸 막음
+    if(toDo === ""){ //toDo가 비어있다면
+      return; //return되도록 해서 함수가 작동하지 않도록 함 (return은 파괴(끝)할때인거같음..)
+    }
+    
+    setToDo("");
+    //toDos.push 자바였다면 이렇게 씀 .push는 배열의 끝에 요소를 추가할때 사용
   };
-  console.log(toDo);
+  console.log(toDos);
   return (
     //submit이벤트
     <div>
       <form onSubmit={onSubmit}>
         <input
-          onChange={onChange}
           value={toDo}
+          onChange={onChange}
           type="text"
           placeholder="Write your to do..."
         />
